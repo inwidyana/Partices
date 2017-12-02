8622fa1e31d1bb325e242c3db0214c2d037e389e
Indra Widyana <indrawidyana@gmail.com>
2017-12-02 09:38:47 +0700

    added recyclerview, dataset not yet

---
+++ app/src/main/java/com/joapps/crowdsource/MyAdapter.java
@@ -0,0 +1,47 @@
+package com.joapps.crowdsource;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by indra on 02/12/17.
+ */
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private String[] mDataset;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextView;
+        public ViewHolder(TextView v) {
+            super(v);
+            mTextView = v;
+        }
+    }
+
+    public MyAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.my_text_view, parent, false);
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mTextView.setText(mDataset[position]);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
--- app/src/main/java/com/joapps/crowdsource/SubPage0.java
+++ app/src/main/java/com/joapps/crowdsource/SubPage0.java
@@ -3,12 +3,16 @@ package com.joapps.crowdsource;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class SubPage0 extends Fragment {
-
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     public SubPage0() {
     }
@@ -24,6 +28,12 @@ public class SubPage0 extends Fragment {
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_thread_feed, container, false);
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
         return rootView;
 
     }
